9ef82eccf2c322193ebdc350953ef7c991f748f7
Yogesh, Ruthik <ruthik.yogesh@accenture.com>
2025-07-15 09:26:55 +0530

    reset password changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./Feature/header/header.component";
+ 
 import { LoadingBarComponent, LoadingBarModule } from '@ngx-loading-bar/core';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
@@ -8,7 +8,7 @@ import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoadingBarModule,LoadingBarRouterModule, LoadingBarHttpClientModule
+  imports: [RouterOutlet , LoadingBarModule,LoadingBarRouterModule, LoadingBarHttpClientModule
       
   ],
   templateUrl: './app.component.html',
